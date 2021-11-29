91df2bdf71f4f5d0318d8380975f5ceecbc1f24e
Adnan Ali <adnanali6688@gmail.com>
2021-11-29 23:28:08 +0100

    added colors, if number is greater than 0, color is green. less than 0, color is red

--- script.js
+++ script.js
@@ -7,17 +7,32 @@ const increase = document.querySelector('.increase');
 
 let count = 0;
 
+const colors = function () {
+  if (count > 0) {
+    number.style.color = 'green';
+  }
+  if (count < 0) {
+    number.style.color = '#940505';
+  }
+  if (count === 0) {
+    number.style.color = 'black';
+  }
+};
+
 increase.addEventListener('click', function () {
   count = count + 1;
   number.textContent = count;
+  colors();
 });
 
 decrease.addEventListener('click', function () {
   count = count - 1;
   number.textContent = count;
+  colors();
 });
 
 reset.addEventListener('click', function () {
   count = 0;
   number.textContent = count;
+  colors();
 });
